340a3be9e0987c750f33314ec4518e5044b3ba44
s88037zz <s88037zz@gmail.com>
2019-12-27 19:00:48 +0800

    Revise let to const in base.js

--- src/Api/base.js
+++ src/Api/base.js
@@ -4,7 +4,7 @@ import axios from "axios";
 async function post(path, data, header) {
 	try {
 		console.log("header=", header);
-		let resp = await axios({
+		const resp = await axios({
 			method: "POST",
 			url: baseURL + path,
 			data: data,
@@ -23,7 +23,7 @@ async function post(path, data, header) {
 
 async function get(path, header) {
 	try {
-		let resp = await axios({
+		const resp = await axios({
 			method: "GET",
 			url: baseURL + path,
 			headers: header
@@ -37,7 +37,7 @@ async function get(path, header) {
 
 async function put(path, data, header) {
 	try {
-		let resp = await axios({
+		const resp = await axios({
 			method: "Post",
 			url: baseURL + path,
 			data: data,
@@ -52,7 +52,7 @@ async function put(path, data, header) {
 
 async function deleteRequest(path, data, header) {
 	try {
-		let resp = await axios({
+		const resp = await axios({
 			method: "Delete",
 			url: baseURL + path,
 			headers: header
@@ -106,14 +106,14 @@ class ApiHelper {
 
 	async login(data) {
 		this.checkHeader()
-		let res = await post("login", data, this.header);
+		const res = await post("login", data, this.header);
 		return checkLogin(res);
 	}
 
 	async getUserName(email) {
 		this.checkHeader()
 		this.path = "user?Mail=" + email;
-		let res = await get(this.path, this.header);
+		const res = await get(this.path, this.header);
 		console.log("in getUserName: ", res.data.userName);
 		return res.data.userName;
 	}
@@ -121,7 +121,7 @@ class ApiHelper {
 	async getCartId(userName) {
 		this.checkHeader()
 		this.path = "getcartidwithusername?UserName=" + userName;
-		let res = await get(this.path, this.header);
+		const res = await get(this.path, this.header);
 		console.log("in getCartid ", res.data.CartId);
 		return res.data.CartId;
 	}
@@ -129,41 +129,41 @@ class ApiHelper {
 	async publishProduct(isStaff, data) {
 		this.checkHeader()
 		if (await isStaff) {
-			let res = await post("sell", data, this.header);
+			const res = await post("sell", data, this.header);
 			return checkPublic(res);
 		}
 	}
 	async getProducts(query) {
 		this.checkHeader()
-		let res = await get("queryproduct" + query, this.header);
+		const res = await get("queryproduct" + query, this.header);
 		console.log("getProducts:", res.data);
 		return res.data;
 	}
 	//---------------Cart---------------------------/
 	async getOrderItems(query) {
 		this.checkHeader()
-		let res = await get("getorderitemsincart" + query, this.header);
+		const res = await get("getorderitemsincart" + query, this.header);
 		console.log("get OrderItems:", res.data);
 		return res.data;
 	}
 
 	async deleteOderItem(data) {
 		this.checkHeader()
-		let res = await post("deleteorderitemincart", data, this.header);
+		const res = await post("deleteorderitemincart", data, this.header);
 		console.log("deleteOderItem:", res);
 		return res;
 	}
 
 	async modifyOrderItem(data) {
 		this.checkHeader()
-		let res = await put("modifyorderitemquantity", data, this.header);
+		const res = await put("modifyorderitemquantity", data, this.header);
 		console.log("modifyOrderItem:", res);
 		return res;
 	}
 
 	//---------------User----------------------//
 	async getUsers(query) {
-		let res;
+		const res;
 		if (query === "") {
 			res = await get("users", this.header);
 		} else {
@@ -175,7 +175,7 @@ class ApiHelper {
 	
 	async deleteUser(data){
 		this.checkHeader()
-		let res = await deleteRequest("user", data, this.header);
+		const res = await deleteRequest("user", data, this.header);
 		console.log("modifyOrderItem:", res);
 		return res;
 	}
